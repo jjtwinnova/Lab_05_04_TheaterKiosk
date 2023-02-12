a60d491409c44503faf08c5cce65c79e0f53cb8f
Jacob <john2jo@mail.uc.edu>
2023-02-12 11:16:05 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Declare Variables
+        int age = 20;
+
+        //Output
+        System.out.println("What is your age? " + age);
+
+        //if statement
+        if(age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
+    }
+}
